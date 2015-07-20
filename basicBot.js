044cd3c2c7800722fcda690260035ef30dce59b6
sukybutzi <sukybutzi@gmail.com>
2015-07-20 23:42:04 +0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1,5 +1,5 @@
 /**
- *Copyright 2014 Yemasthui
+ *Copyright 2015-2016 PaianganBot
  *Modifications (including forks) of the code to fit personal needs are allowed only for personal use and should refer back to the original source.
  *This software is not for profit, any extension, or unauthorised person providing this software is not authorised to be in a position of any monetary gain from this use of this software. Any and all money gained under the use of the software (which includes donations) must be passed on to the original author.
  */
@@ -24,7 +24,7 @@
         }
         return -1;
     };
-var paianganbot = {};
+//var paianganbot = {};
     var kill = function () {
         clearInterval(basicBot.room.autodisableInterval);
         clearInterval(basicBot.room.afkInterval);
