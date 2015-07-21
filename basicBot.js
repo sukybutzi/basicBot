056a772f5a1d5ab7b2be9e1bf56f0145b31d1496
sukybutzi <sukybutzi@gmail.com>
2015-07-21 17:43:35 +0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1,6 +1,5 @@
 /**
  *Copyright 2015-2016 PaianganBot by Suky
- *The main script is the basicBot by Yemasthui
  *Modifications (including forks) of the code to fit personal needs are allowed only for personal use and should refer back to the original source.
  *This software is not for profit, any extension, or unauthorised person providing this software is not authorised to be in a position of any monetary gain from this use of this software. Any and all money gained under the use of the software (which includes donations) must be passed on to the original author.
  */
@@ -243,7 +242,7 @@ paianganbot.handle.upboat = function(data){
     var botCreatorIDs = ["8058328", "4856169"];
 
     var basicBot = {
-        version: "1.0.3",
+        version: "1.0.2",
         status: false,
         name: "PaianganBot",
         loggedInID: null,
