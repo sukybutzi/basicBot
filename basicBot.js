9aa349c228e03a52ff9d6fb3d270e7d7cd10ec33
sukybutzi <sukybutzi@gmail.com>
2015-07-26 00:49:50 +0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2574,7 +2574,7 @@ paianganbot.handle.upboat = function(data){
 
             linkCommand: {
                 command: 'link',
-                rank: 'user',
+                rank: 'residentdj',
                 type: 'exact',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
